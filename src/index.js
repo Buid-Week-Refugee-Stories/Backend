2f6f1f9d17c52a90bb67e44b8ed11f2e82f4cd16
BaoPham92 <baopham92@yahoo.com>
2019-11-19 18:51:31 -0500

    Ping for Heroku server

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ const port = process.env.PORT || 4000;
 const dynoPing = require('./API/Utils/dynoPing');
 
 process.env.DB_ENV &&
-server.listen(port, () => dynoPing('bw-refugees.herokuapp.com'))
+server.listen(port, () => dynoPing('bw-refugees.herokuapp.com'));
 
 !process.env.DB_ENV && process.env.PORT &&
-server.listen(port, () => console.log(`Currently serving BW-Refugees on ${port}`))
+server.listen(port, () => console.log(`Currently serving BW-Refugees on ${port}`));
