5a44666659875558b8e8e0065998f16b2ab6d72a
BaoPham92 <baopham92@yahoo.com>
2019-11-18 22:29:56 -0500

    Minor adjustmnet: Nullabble()

--- src/Data/migrations/20191116160639_main.js
+++ src/Data/migrations/20191116160639_main.js
@@ -4,23 +4,37 @@ exports.up = function (knex) {
             tbl.increments();
 
             // * KEV-VALUES 
-            tbl.string('username', 100).notNullable().unique();
-            tbl.string('password', 255).notNullable();
+            tbl.string('username', 100)
+                .notNullable()
+                .unique();
+            tbl.string('password', 255)
+                .notNullable();
             tbl.string('location', 50);
-            tbl.boolean('admin').notNullable().defaultTo(false);
-            tbl.timestamp('created_at').defaultTo(knex.fn.now());
+            tbl.boolean('admin')
+                .notNullable()
+                .defaultTo(false);
+            tbl.timestamp('created_at')
+                .defaultTo(knex.fn.now());
         })
         .createTable('stories', tbl => {
             tbl.increments();
 
             // * KEV-VALUES 
-            tbl.string('story_title', 255).notNullable();
-            tbl.string('story_description', 255).notNullable();
-            tbl.boolean('approved_story').notNullable().defaultTo(false);
+            tbl.string('story_title', 255)
+                .notNullable();
+            tbl.string('story_description', 255)
+                .notNullable();
+            tbl.boolean('approved_story')
+                .notNullable()
+                .defaultTo(false);
             tbl.string('author', 100);
             tbl.string('location', 50);
-            tbl.string('avatar_image', 255).defaultTo('https://source.unsplash.com/random/500x550/?people');
-            tbl.string('refugee_location_img', 255).defaultTo('https://source.unsplash.com/random/1440x600/?landscape');
+            tbl.string('avatar_image', 255)
+                .notNullable()
+                .defaultTo('https://source.unsplash.com/random/500x550/?people');
+            tbl.string('refugee_location_img', 255)
+                .notNullable()
+                .defaultTo('https://source.unsplash.com/random/1440x600/?landscape');
             tbl.timestamp('created_at').defaultTo(knex.fn.now());
         })
 };
