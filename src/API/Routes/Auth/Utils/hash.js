0fdb92ce37c53fb4c6d2df08940b093e4b8a8095
BaoPham92 <baopham92@yahoo.com>
2019-11-19 13:17:19 -0500

    Minor adjustment

--- src/API/Routes/Auth/Utils/hash.js
+++ src/API/Routes/Auth/Utils/hash.js
@@ -2,7 +2,7 @@ const bcrypt = require('bcryptjs');
 
 const hashPassword = (password) => {
     if (password.length < 1) {
-        throw new Error('Password must be 8 characters or longer.')
+        throw new Error('Password must be at least 1 characters or longer.')
     }
 
     return bcrypt.hashSync(password, 10)
