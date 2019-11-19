d75d1c3e24cb9ab3e66b28a2bd480d80972adc71
BaoPham92 <baopham92@yahoo.com>
2019-11-19 15:31:15 -0500

    Switching to SQLite3

--- knexfile.js
+++ knexfile.js
@@ -14,11 +14,11 @@ module.exports = {
     }
   },
 
-  testing: {
+  production: {
     client: 'sqlite3',
     useNullAsDefault: true,
     connection: {
-      filename: './src/Data/DataBases/test.db3'
+      filename: './src/Data/DataBases/bw-refugees.db3'
     },
     migrations: {
       directory: './src/Data/migrations',
@@ -28,15 +28,15 @@ module.exports = {
     }
   },
 
-  production: {
-    client: 'pg',
-    connection: process.env.DATABASE_URL,
-    migrations: {
-      directory: './src/Data/migrations',
-    },
-    seeds: {
-      directory: './src/Data/seeds',
-    }
-  }
+  // production: {
+  //   client: 'pg',
+  //   connection: process.env.DATABASE_URL,
+  //   migrations: {
+  //     directory: './src/Data/migrations',
+  //   },
+  //   seeds: {
+  //     directory: './src/Data/seeds',
+  //   }
+  // }
 
 };
